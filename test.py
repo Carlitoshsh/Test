026113918394d649f32d60f407847dccc02e589c
Carlos Gómez <caragomezher@unal.edu.co>
2023-07-05 20:58:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import os
+import subprocess
 
 def delete_git_folders(root_dir):
     for root, dirs, files in os.walk(root_dir):
         if '.git' in dirs:
             git_dir = os.path.join(root, '.git')
             print(f"Deleting {git_dir}")
-            # Uncomment the line below to actually delete the .git folder
-            # shutil.rmtree(git_dir)
+            subprocess.run(["rd", "/s", "/q", git_dir], shell=True)
 
 root_directory = r"C:\tmp\projects"
 
