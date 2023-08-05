2f5f6cb57d43daa5d9b5536ad15397bf95348fb0
Carlos Gomez <caragomezher@unal.edu.co>
2023-08-05 14:12:09 -0500

    Test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  function redirectToGiftPage() {
+    window.location.href = '/gift';
+  }
+</script>
+
+<div class="content">
+    <div class="kuromi"></div>
+    <h1 class="happy-birthday">Feliz Cumple SAAAAAAAM!</h1>
+    <div class="kuromi2" on:click={redirectToGiftPage}></div>
+</div>
---
+++ src/routes/gift/+page.svelte
@@ -0,0 +1,6 @@
+
+<div class="content">
+    <a href="/">Volver</a>
+    <div class="kuromi2"></div>
+</div>
+
