9b11ccf2a5e74cce549d49d2a5aad0e25f4d0922
Carlos Gómez <caragomezher@unal.edu.co>
2023-08-05 19:13:26 -0500

    Add updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,6 @@
 
 <div class="content">
     <div class="kuromi"></div>
-    <h1 class="happy-birthday">Feliz Cumple SAAAAAAAM!</h1>
+    <h1 class="happy-birthday">¡Feliz Cumple SAAAAAAAM!</h1>
     <div class="kuromi2" on:click={redirectToGiftPage}></div>
 </div>
--- src/routes/gift/+page.svelte
+++ src/routes/gift/+page.svelte
@@ -1,6 +1,27 @@
-
 <div class="content">
+  <div class="gift">
     <a href="/">Volver</a>
-    <div class="kuromi2"></div>
+    <div class="kuromi2" />
+    <h2>Te gusta la música, ¡aquí algunas opciones!</h2>
+    <div class="grid-container">
+      <a
+        class="grid-item"
+        href="https://www.tuboleta.com/images/Eventos/primavera-sound-2023-bogota/index.html?_gl=1*1vewpit*_ga*MjA2NTkwMDc3MS4xNjUyMTI3MDg0*_ga_0TVTJ30NVQ*MTY4OTE2ODExNC43MjYuMS4xNjg5MTY4MTE5LjU1LjAuMA"
+      >
+        <img src="images/vendor/primavera.jpg" alt="primavera-sound" />
+      </a>
+      <a
+        class="grid-item"
+        href="https://taqui.shop/v2/#/teatro/metropolitano/6893"
+      >
+        <img src="images/vendor/vetusta.webp" alt="vetusta" />
+      </a>
+      <a
+        class="grid-item"
+        href="https://www.eticket.co/eventos.aspx?idartista=580"
+      >
+        <img src="images/vendor/cordillera.jpg" alt="cordillera" />
+      </a>
+    </div>
+  </div>
 </div>
-
