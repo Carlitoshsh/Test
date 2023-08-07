53feeb57c649c35d49c01f64feee82a5e7e60c05
Carlos Gómez <caragomezher@unal.edu.co>
2023-08-06 19:14:20 -0500

    Finished!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,25 @@
+<script>
+  import { fade } from 'svelte/transition';
+
+  let firstMessage = "¡Feliz cumple Saaaam 🦊💟!"
+  let currentText = firstMessage;
+  let newText = '¡26 bonis!';
+
+  function toggleText() {
+    currentText = currentText === firstMessage ? newText : firstMessage;
+  }
+
+  const interval = setInterval(toggleText, 3999);
+</script>
 
 <div class="content">
-    <a href="/coupon" class="link-main">
-      <div class="kuromi"></div>
-    </a>
-    <h1 class="happy-birthday">¡Feliz Cumple SAAAAAAAM!</h1>
-    <a href="/gift" class="link-main">
-      <img src="/images/kuromi2.gif" alt="Kuromi">
-    </a>
+  <a href="/coupon" class="link-main">
+    <div class="kuromi" />
+  </a>
+  <h1 class="happy-birthday" transition:fade>
+    {currentText}
+  </h1>
+  <a href="/gift" class="link-main">
+    <img src="/images/kuromi2.gif" alt="Kuromi" />
+  </a>
 </div>
